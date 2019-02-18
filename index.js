25313a263df9b522fd82703d34c838ba88c80d63
Ricardo Villar <ricardo.villar@dbsoft.com.br>
2019-02-18 18:24:40 -0300

    Desmembramento do SDK

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-// module.exports.dates = require("./lib/utils/dates");
-// module.exports.objects = require("./lib/utils/tokens");
 module.exports.passwords = require("./lib/utils/passwords");
-// module.exports.sqls = require("./lib/utils/tokens");
-// module.exports.tokens = require("./lib/utils/tokens");
-// module.exports.validators = require("./lib/utils/validators");
-// module.exports.numbers = require("./lib/utils/numbers");
-// module.exports.invests = require("./lib/utils/invests");
-// module.exports.httpRequests = require("./lib/utils/httpRequests");
+module.exports.sqls = require("./lib/utils/sqls");
+module.exports.tokens = require("./lib/utils/tokens");
 
-// module.exports.crud = require("./lib/hofs/crud");
-// module.exports.dao = require("./lib/hofs/dao");
-// module.exports.model = require("./lib/hofs/model");
-// module.exports.mySqlServer = require("./lib/dbs/mySqlServer");
+module.exports.dao = require("./lib/hofs/dao");
+module.exports.crud = require("./lib/hofs/crud");
 
-// module.exports.httpCors = require("./lib/middlewares/httpCors");
-// module.exports.hostsCheckPoint = require("./lib/middlewares/hostsCheckPoint");
+module.exports.mySqlServer = require("./lib/dbs/mySqlServer");
 
-// module.exports.requestContext = require("./lib/middlewares/requestContext").requestContext;
-// module.exports.requestContextEvents = require("./lib/middlewares/requestContext").requestContextEvents;
-// module.exports.requestContextMiddleware = require("./lib/middlewares/requestContext").requestContextMiddleware;
+module.exports.httpCors = require("./lib/middlewares/httpCors");
+module.exports.hostsCheckPoint = require("./lib/middlewares/hostsCheckPoint");
+
+module.exports.requestContext = require("./lib/middlewares/requestContext").requestContext;
+module.exports.requestContextEvents = require("./lib/middlewares/requestContext").requestContextEvents;
+module.exports.requestContextMiddleware = require("./lib/middlewares/requestContext").requestContextMiddleware;
 
 //GLobals-------------------------
 //Log
-// gLog = require("./lib/log");
+gLog = require("./lib/log");
