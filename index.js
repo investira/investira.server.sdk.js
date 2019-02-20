ef1721542e104afb2c5ebe021d03400e5f463722
Ricardo Villar <ricardo.villar@dbsoft.com.br>
2019-02-20 14:38:15 -0300

    Documentação

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-module.exports.passwords = require("./lib/utils/passwords");
-module.exports.sqls = require("./lib/utils/sqls");
-module.exports.tokens = require("./lib/utils/tokens");
+module.exports.passwords = require('./lib/utils/passwords');
+module.exports.sqls = require('./lib/utils/sqls');
+module.exports.tokens = require('./lib/utils/tokens');
 
-module.exports.dao = require("./lib/hofs/dao");
-module.exports.crud = require("./lib/hofs/crud");
+module.exports.dao = require('./lib/hofs/dao');
+module.exports.crud = require('./lib/hofs/crud');
 
-module.exports.mySqlServer = require("./lib/dbs/mySqlServer");
+module.exports.mySqlServer = require('./lib/dbs/mySqlServer');
 
-module.exports.httpCors = require("./lib/middlewares/httpCors");
-module.exports.hostsCheckPoint = require("./lib/middlewares/hostsCheckPoint");
+module.exports.httpCors = require('./lib/middlewares/httpCors');
+module.exports.hostsCheckPoint = require('./lib/middlewares/hostsCheckPoint');
 
-module.exports.requestContext = require("./lib/middlewares/requestContext").requestContext;
-module.exports.requestContextEvents = require("./lib/middlewares/requestContext").requestContextEvents;
-module.exports.requestContextMiddleware = require("./lib/middlewares/requestContext").requestContextMiddleware;
+module.exports.requestContext = require('./lib/middlewares/requestContext').requestContext;
+module.exports.requestContextEvents = require('./lib/middlewares/requestContext').requestContextEvents;
+module.exports.requestContextMiddleware = require('./lib/middlewares/requestContext').requestContextMiddleware;
 
 //GLobals-------------------------
 //Log
-gLog = require("./lib/log");
+gLog = require('./lib/log');
